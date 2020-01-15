a7e24aba9fec00c013fba772344ed3fe8aba6fe9
Hyblocker <bryan.spiteri@gmail.com>
2020-01-15 13:54:52 +0100

    Initial Chunk Handling

---
+++ Endless Runner/Assets/Scripts/ChunkHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		Debug.Log("FUCK I DIDNT WRITE CODE FUCKING SHIT FUCK");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

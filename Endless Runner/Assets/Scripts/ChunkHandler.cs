9814318c7cc2234a43041498e6bdf19a4ff59a42
Hyblocker <bryan.spiteri@gmail.com>
2020-01-22 14:49:07 +0100

    Fixed gitignore

--- Endless Runner/Assets/Scripts/ChunkHandler.cs
+++ Endless Runner/Assets/Scripts/ChunkHandler.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class ChunkHandler : MonoBehaviour
 {
+	public GameObject ChunkPool;
+	public GameObject ChunkPrefab;
+
+	public ushort RenderDistance;
+
+	public Transform PlayerTransform;
+	public long ChunkID = 0x00;
+	public long PreviousChunkID = 0x00;
+
     // Start is called before the first frame update
     void Start()
     {
-		Debug.Log("FUCK I DIDNT WRITE CODE FUCKING SHIT FUCK");
+		ChunkID = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+		//TODO: Remove auto moving player
+		PlayerTransform.position = new Vector3(PlayerTransform.position.x, PlayerTransform.position.y, PlayerTransform.position.z + 1f * Time.deltaTime);
+
+		//Calculate the current chunk
+
+
+		//Remember the previous chunk
+		PreviousChunkID = ChunkID;
+	}
 }

6bc31486010a3e92e610a6a0d3e86a67f105e2d8
Hyblocker <bryan.spiteri@gmail.com>
2020-02-12 14:36:31 +0100

    Lots of things. im too lazy to describe lol

--- Endless Runner/Assets/Scripts/ChunkHandler.cs
+++ Endless Runner/Assets/Scripts/ChunkHandler.cs
@@ -1,42 +1,56 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ChunkHandler : MonoBehaviour
 {
+	//Chunk References
 	public GameObject ChunkPool;
 	public GameObject ChunkPrefab;
 
+	//Render Distance
 	public ushort RenderDistance;
+	public ushort RenderDistanceBack;
 
+	//References
 	public Transform PlayerTransform;
-	public long ChunkID = 0x00;
-	public long PreviousChunkID = 0x00;
+	public float ChunkID = 0x00;
+	public float PreviousChunkID = 0x00;
 
+	//Debug Shit
 	public Text DebugUI;
 	public float Speed = 1f;
 
+	[HideInInspector]
 	public float ChunkScale = 0;
 
-    	// Start is called before the first frame update
-	void Start()
-    	{
+    // Start is called before the first frame update
+    void Start()
+    {
 		ChunkID = 0;
 		ChunkScale = ChunkPrefab.transform.localScale.x;
+
+		//Initialise the first 5 chunks
+		for (int i = -RenderDistanceBack; i < RenderDistance + RenderDistanceBack; i++)
+		{
+			ChunkID = i;
+			InstantiateChunk();
+		}
 	}
-	
-    	// Update is called once per frame
-    	void Update()
-    	{
+
+    // Update is called once per frame
+    void Update()
+    {
 		//TODO: Remove auto moving player
 		PlayerTransform.position = new Vector3(PlayerTransform.position.x, PlayerTransform.position.y, PlayerTransform.position.z + Speed * Time.deltaTime);
 
 		//Calculate the current chunk
-		ChunkID = (long) (PlayerTransform.position.z / 16L);
-		if (ChunkID > PreviousChunkID)
+		ChunkID = (PlayerTransform.position.z / (ChunkScale * 2));
+		if ((int)ChunkID > (int)PreviousChunkID)
 		{
 			InstantiateChunk();
+			RemoveChunk();
 		}
 
 		DebugUI.text = "ChunkID: " + ChunkID;
@@ -47,21 +61,34 @@ public class ChunkHandler : MonoBehaviour
 
 	public void InstantiateChunk()
 	{
-		GameObject ChunkTmp = Instantiate(ChunkPrefab);
-		ChunkTmp.name = "Chunk-" + (ChunkID + RenderDistance);
-		ChunkTmp.transform.parent = ChunkPool.transform;
-		ChunkTmp.transform.position = new Vector3(0, 0, ChunkScale * (ChunkID + 1 + RenderDistance));
-		Debug.Log("ChunkScale * (" + ChunkID + " + 1 + " + RenderDistance + ")");
+		bool exists = false;
+		Transform[] chunks = ChunkPool.transform.GetComponentsInChildren<Transform>();
+		for (int i = 0; i < chunks.Length; i++)
+		{
+			//Render Distance behind check
+			if (chunks[i].name == "Chunk-" + ((int)ChunkID + RenderDistance))
+			{
+				exists = true;
+			}
+		}
+		if (exists == false)
+		{
+			GameObject ChunkTmp = Instantiate(ChunkPrefab);
+			ChunkTmp.name = "Chunk-" + ((int)ChunkID + RenderDistance);
+			ChunkTmp.transform.parent = ChunkPool.transform;
+			ChunkTmp.transform.position = new Vector3(0, 0, ChunkScale * 2 * ((int)ChunkID + 1 + RenderDistance));
+		}
 	}
 
 	public void RemoveChunk()
 	{
-		Transform[] chunks = ChunkPool.transform.GetComponentsInParent<Transform>();
+		Transform[] chunks = ChunkPool.transform.GetComponentsInChildren<Transform>();
 		for (int i = 0; i < chunks.Length; i++)
 		{
-			if (chunks[i].name == "Chunk-" + ChunkID)
+			//Render Distance behind check
+			if (chunks[i].name == "Chunk-" + ((int)ChunkID - RenderDistanceBack))
 			{
-				Destroy(chunks[i]);
+				chunks[i].GetComponent<Destroyer>().Remove();
 			}
 		}
 	}

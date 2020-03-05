8d656deb8b00d7f9e4fd0d7f12e3a7ffb442827e
LukeZerafa <luke.look1@gmail.com>
2020-03-05 22:24:07 +0100

    added death screen and menu

---
+++ Endless Runner/Assets/Scripts/MainMenuScreen.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class MainMenuScreen : MonoBehaviour
+{
+     public void playButtonClicked()
+    {
+        SceneManager.LoadScene("GameLevel");
+    }
+}
